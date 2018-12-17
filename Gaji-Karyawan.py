7da0bb58b04a7a5f2a770eaba0591f1f14b653b6
nurul021 <nh763902@gmail.com>
2018-12-17 11:48:45 +0700

    second master

--- Gaji-Karyawan.py
+++ Gaji-Karyawan.py
@@ -1,3 +1,13 @@
 print ("=========================================")
 print ("      Aplikasi Penggajian Karyawan")
 print ("=========================================")
+
+nama = str(input("nama pegawai :"))
+golongan = str(input("golongan :"))
+awal = int(input("tahun awal bekerja :"))
+akhir = int(input("habis kontrak :"))
+gapok = int(input("gaji pokok :"))
+jam = int(input("jam per hari bekerja :"))
+hari = int(input("jmlh hari bekerja :"))
+anak = int(input("jmlh anak :"))
+kerja = 9+jam 
